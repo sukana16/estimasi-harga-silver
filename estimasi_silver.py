ee7a981248de477107f922d87c033d2abc85fffd
DWI FRISTTIKASARI <108009651+sukana16@users.noreply.github.com>
2023-04-29 22:57:07 +0700

    Update estimasi_silver.py

--- estimasi_silver.py
+++ estimasi_silver.py
@@ -8,14 +8,13 @@ st.title('Estimasi Harga silver ')
 Open = st.number_input('Input Harga Open')
 High = st.number_input('Input Harga High')
 Low = st.number_input('Input Harga Low')
-Close = st.number_input('Input Harga Close')
 Volume = st.number_input('Input Volume')
 
 predict = ''
 
 if st.button('Estimasi Harga'):
     predict = model.predict(
-        [[year, mileage, tax, mpg, engineSize]]
+        [[Open, High, Low, Volume]]
     )
     st.write ('Estimasi harga silver dalam USD : ', predict)
-    st.write ('Estimasi harga silver dalam IDR (Juta) :', predict*19000)
+    st.write ('Estimasi harga silver dalam IDR (Juta) :', predict*15000)
